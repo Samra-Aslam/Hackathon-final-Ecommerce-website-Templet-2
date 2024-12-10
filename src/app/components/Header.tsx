9a27a66f248301d54a84458d70a2e05e63a72760
Samra-Aslam <164459427+Samra-Aslam@users.noreply.github.com>
2024-12-10 00:40:49 -0800

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { CiSearch } from "react-icons/ci";
 import { IoCartOutline } from "react-icons/io5";
 import { MdOutlineAccountCircle } from "react-icons/md";
+import Link from 'next/link';
 
 const Header = () => {
   return (
@@ -10,7 +11,7 @@ const Header = () => {
       <div className='w-[100%] h-[auto] flex justify-around pt-[20px] pb-[20px]'>
            <div className='text-[24px]'><CiSearch /></div>
            <div className='text-[24px] w-[65px] h-[30px] font-normal'>Avion</div>
-           <div className='flex justify-between text-[24px]'><h3 className='mr-[10px]'><IoCartOutline /></h3><h3><MdOutlineAccountCircle /></h3></div>
+           <div className='flex justify-between text-[24px]'><h3 className='mr-[10px]'><Link href='/cart'><IoCartOutline /></Link></h3><h3><MdOutlineAccountCircle /></h3></div>
       </div>
       <hr></hr>
       {/* lower part */}
